f8b5520724b97b3f939fce0a29ad74e3fc3e1048
Mateusz Twardowski <matit546@gmail.com>
2021-03-28 20:20:56 +0200

    Add Movement for Character
    Third person movement for main character

---
+++ Secrets of The Lost Lands/Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform cam;
+    public Transform groundCheck;
+    public LayerMask groundMask;
+
+    public float speed = 6f;
+    public float turnSmoothTime = 0.1f;
+    public float gravity = -9.81f;
+    public float jumpHeigt=3f;
+    
+    public float groundDistance =1f;
+    public bool isGrounded;
+    Vector3 velocity;
+    float turnSmoothVelocity;
+    void Update()
+    {
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y <=.3f)
+        {
+            velocity.y = .15f;
+            isGrounded = true;
+        }
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeigt * -2f * gravity);
+        }
+
+        if (!isGrounded)
+        {
+            velocity.y += gravity * Time.deltaTime;
+        }
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
