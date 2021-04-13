5c8245051d037b908efae6fb02a0d4195558e3a0
Mateusz Twardowski <matit546@gmail.com>
2021-04-13 12:43:21 +0200

    PauseControl Script
    Script to pause the game by clicking escape.

--- Secrets of The Lost Lands/Assets/Scripts/ExitApplicationScript.cs
+++ Secrets of The Lost Lands/Assets/Scripts/ExitApplicationScript.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class ExitApplicationScript : MonoBehaviour
 {
-    public void ExitApplication()
+
+    public void  ExitApplication()
     {
         Application.Quit();
     }
---
+++ Secrets of The Lost Lands/Assets/Scripts/PauseControl.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseControl : MonoBehaviour
+{
+
+    public static bool isGamePaused;
+    public GameObject pauseMenuUI;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isGamePaused ^= true;
+
+            if (isGamePaused)
+            {
+                PauseGame();
+            }
+            else
+            {
+                ResumeGame();
+            }
+       
+        }
+    }
+
+    void PauseGame()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void ResumeGame()
+    {
+        Cursor.visible = false;
+        isGamePaused = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+}
