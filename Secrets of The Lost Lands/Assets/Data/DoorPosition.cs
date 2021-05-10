cdac3c9bf8d79166685cd29536fe4765f8e3c924
JerzyDlugosz <dlugoszjerzy98@gmail.com>
2021-05-10 14:50:43 +0200

    Add Lever Puzzle

---
+++ Secrets of The Lost Lands/Assets/Data/DoorPosition.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorPosition : MonoBehaviour
+{
+    public bool isOpen = false;
+}
--- Secrets of The Lost Lands/Assets/Scripts/OpenDoors.cs
+++ Secrets of The Lost Lands/Assets/Scripts/OpenDoors.cs
@@ -4,22 +4,52 @@ using UnityEngine;
 
 public class OpenDoors : MonoBehaviour
 {
-    public Transform door;
-    private bool isOpen = false;
+    [SerializeField]
+    public List<DoorClass> doors;
 
     public void OpenCloseDoor()
     {
+        foreach(DoorClass door in doors)
+        {
+            if (door.doorPosition.isOpen)
+            {
+                StartCoroutine(CloseDoor(door));
+                door.doorPosition.isOpen = false;
+            }
+            else
+            {
+                StartCoroutine(OpenDoor(door));
+                door.doorPosition.isOpen = true;
+            }
+        }
+    }
 
-        if (isOpen)
+    public IEnumerator OpenDoor(DoorClass door)
+    {
+        for (int i = 0; i < 100; i++)
         {
-            door.Translate(0, -2f, 0);
-            isOpen = false;
+            door.transform.Translate(0, door.doorOpenRange / 100, 0);
+            yield return null;
         }
-        else
+    }
+
+    public IEnumerator CloseDoor(DoorClass door)
+    {
+        for (int i = 0; i < 100; i++)
         {
-            door.Translate(0, 2f, 0);
-            isOpen = true;
+            door.transform.Translate(0, -door.doorOpenRange / 100, 0);
+            yield return null;
         }
-        
+    }
+
+    [System.Serializable]
+    public class DoorClass
+    {
+        [SerializeField]
+        public Transform transform;
+        [SerializeField]
+        public DoorPosition doorPosition;
+        [SerializeField]
+        public float doorOpenRange = 3;
     }
 }
